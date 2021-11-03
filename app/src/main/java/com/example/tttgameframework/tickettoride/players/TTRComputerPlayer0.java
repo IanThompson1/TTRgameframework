408c20e075b1030b28607175c4866aaf2a940060
baird24 <baird24@up.edu>
2021-11-03 14:30:51 -0700

    Started computer player

--- app/src/main/java/com/example/tttgameframework/tickettoride/players/TTRComputerPlayer0.java
+++ app/src/main/java/com/example/tttgameframework/tickettoride/players/TTRComputerPlayer0.java
@@ -2,8 +2,14 @@ package com.example.tttgameframework.tickettoride.players;
 
 import com.example.tttgameframework.GameFramework.infoMessage.GameInfo;
 import com.example.tttgameframework.GameFramework.players.GameComputerPlayer;
+import com.example.tttgameframework.tickettoride.infoMessage.TTRState;
+import com.example.tttgameframework.tickettoride.ttrActionMessage.PlaceTrains;
+
+import java.util.Random;
 
 public class TTRComputerPlayer0 extends GameComputerPlayer {
+
+    private static final Random rand = new Random();
     /**
      * constructor
      *
@@ -15,6 +21,23 @@ public class TTRComputerPlayer0 extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
+        TTRState state = (TTRState) info;
+
     //if its my turn take a turn
+        if (state.getTurn() != playerNum){  //not sure
+            return;
+        }
+        else {
+            //if picking orignal ticket
+
+            if (rand.nextInt(2) == 1){
+                game.sendAction(new PlaceTrains(this));
+                //send: state.getAllPaths().get(rand.nextInt(state.getAllPaths().size))
+                //select all wilds and what color to use
+            }
+            else{
+                //do
+            }
+        }
     }
 }
