a08ee38541b418aa881ebd9597503b437e25fe7b
baird24 <baird24@up.edu>
2021-11-05 12:07:08 -0700

    Dumb computer player card drawing

--- app/src/main/java/com/example/tttgameframework/tickettoride/players/TTRComputerPlayer0.java
+++ app/src/main/java/com/example/tttgameframework/tickettoride/players/TTRComputerPlayer0.java
@@ -3,8 +3,10 @@ package com.example.tttgameframework.tickettoride.players;
 import com.example.tttgameframework.GameFramework.infoMessage.GameInfo;
 import com.example.tttgameframework.GameFramework.players.GameComputerPlayer;
 import com.example.tttgameframework.tickettoride.infoMessage.TTRState;
+import com.example.tttgameframework.tickettoride.ttrActionMessage.DrawTrains;
 import com.example.tttgameframework.tickettoride.ttrActionMessage.PlaceTrains;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class TTRComputerPlayer0 extends GameComputerPlayer {
@@ -36,7 +38,36 @@ public class TTRComputerPlayer0 extends GameComputerPlayer {
                 //select all wilds and what color to use
             }
             else{
-                //do
+                ArrayList<Boolean> selectedCards = new ArrayList<Boolean>();
+                for (int i = 0; i < 7; i++){
+                    selectedCards.add(false);
+                }
+
+                //do card draw
+                if (rand.nextInt(2) == 1){
+                    //draw face up cards
+                    int card1 = rand.nextInt(5) + 2;
+                    int card2 = rand.nextInt(5) + 2;
+
+                    //check for duplicates
+                    while (card1 == card2){
+                        card2 = rand.nextInt(5) + 2;
+                    }
+
+                    //set which cards to draw
+                    for (int i = 0; i < 7; i++){
+                        if (i == card1 || i == card2){
+                            selectedCards.set(i, true);
+                        }
+                    }
+                }
+
+                else{
+                    //take two random cards
+                    selectedCards.set(0, true);
+                    selectedCards.set(1, true);
+                }
+                game.sendAction(new DrawTrains(this, selectedCards));
             }
         }
     }
