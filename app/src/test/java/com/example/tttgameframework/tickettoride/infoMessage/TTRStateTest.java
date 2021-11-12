8b78a980c7b2866e9d939b67dfee9a8f29755c95
Jennifer Brana <jenniferbrana1@gmail.com>
2021-11-11 16:23:46 -0800

    test

---
+++ app/src/test/java/com/example/tttgameframework/tickettoride/infoMessage/TTRStateTest.java
@@ -0,0 +1,100 @@
+package com.example.tttgameframework.tickettoride.infoMessage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TTRStateTest {
+
+    @Test
+    public void getCardDeck() {
+    }
+
+    @Test
+    public void setCardDeck() {
+    }
+
+    @Test
+    public void getTickets() {
+    }
+
+    @Test
+    public void addTicket() {
+    }
+
+    @Test
+    public void ticketDeckEmpty() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void getPlayer1() {
+    }
+
+    @Test
+    public void getPlayers() {
+    }
+
+    @Test
+    public void getPath() {
+    }
+
+    @Test
+    public void getAllPaths() {
+    }
+
+    @Test
+    public void getTurn() {
+    }
+
+    @Test
+    public void getNumPlayers() {
+    }
+
+    @Test
+    public void getShownTickets() {
+    }
+
+    @Test
+    public void addShownTicket() {
+    }
+
+    @Test
+    public void removeShownTicket() {
+    }
+
+    @Test
+    public void clearShownTickets() {
+    }
+
+    @Test
+    public void getFaceUp() {
+    }
+
+    @Test
+    public void setFaceUp() {
+    }
+
+    @Test
+    public void getWhosTurn() {
+    }
+
+    @Test
+    public void setWhosTurn() {
+    }
+
+    @Test
+    public void addCard() {
+    }
+
+    @Test
+    public void ticket_completed() {
+    }
+
+    @Test
+    public void dfs() {
+    }
+}
